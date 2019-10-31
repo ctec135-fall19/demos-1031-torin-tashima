a2245b01ceb258871de93edef16b68e28ec1dcad
Torin Tashima <t.tashima@students.clark.edu>
2019-10-31 15:14:31 -0700

    Demo Submission 10/31

---
+++ Demo1031/MyMath1.UnitTests/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using MyMath1ConsoleApp;
+
+namespace MyMath1.UnitTests
+{
+    [TestFixture]
+    public class MyMath1Tests
+    {
+        [Test]
+        public void Add_SumWithinByteRange_ReturnCorrectSum()
+        {
+            Assert.AreEqual(200, MyMath.MyMath1.Add(101, 99));
+        }
+        [Test]
+        public void Add_SumOutsideByteRange_ReturnIncorrectSum()
+        {
+            Assert.AreEqual(302, MyMath.MyMath1.Add(101, 201));
+        }
+        public void Add_SumWithinByteRange_AlternateAssert()
+        {
+            Assert.That(MyMath.MyMath1.Add(101, 99), Is.EqualTo(200));
+        }
+        public void Add_SumOutsideByteRange_ThrowsException()
+        {
+            Assert.That(() => MyMath.MyMath1.Add(101, 201), Throws.Exception);
+        }
+    }
+}
---
+++ Demo1031/ReadAndQuery/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Xml;
+using System.Xml.Linq;
+using static System.Console;
+
+namespace ReadAndQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Loading an XML doc");
+
+            XDocument myDoc = XDocument.Load("Inventory.xml");
+            WriteLine(myDoc);
+            WriteLine();
+
+            var result = from car in myDoc.Descendants("Car")
+                         where ((string)car.Attribute("ID")).Equals("2")
+                         select car;
+
+            foreach (var car in result)
+            {
+                WriteLine(car);
+            }
+        }
+    }
+}
